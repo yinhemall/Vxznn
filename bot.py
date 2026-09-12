61679ebfe7a23ab6ad8c5b9caa3e4a95a65b6693
yinhemall <yuzen109@gmail.com>
2026-09-12 04:50:06 +0000

    1

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
-
+import discord
+import os
+bot = discord.Bot()
+@bot.slash_command(name="hello")
+async def hello(ctx):
+    await ctx.respond("Hi,im gay")
+    token = os.getenv("DISCORD_TOKEN")
+    @bot.event
+    async def on_ready():
+        print("✅")
+    bot.run(token)
