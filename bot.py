0661e2c227d5a10d2c17792bb36eb3d217d2c1ce
yinhemall <yuzen109@gmail.com>
2026-09-13 09:49:02 +0000

    1

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import os
 import discord
+import asyncio
 from discord.ext import commands
 from datetime import timedelta
+from discord import app_commands
 
 token = os.getenv("DISCORD_TOKEN")
 
@@ -12,7 +14,9 @@ bot = commands.Bot(command_prefix=["!", "."], intents=intents)
 
 @bot.event
 async def on_ready():
+    synced = await bot.tree.sync()
     print("✅")
+    print(f"以同步 {len(synced)}個指令")
 
 @bot.command()
 async def hello(ctx):
@@ -59,12 +63,83 @@ async def mute_error(ctx, error):
             embed.description = "# ❌MISSING ARGUMENT\n\n**Right Usage: .mute <@user> minutes**"
             await ctx.send(embed=embed)
 
-@bot.slash_command(name="ping")
-async def ping(cxt):
-        latency = bot.latency
-        latency_ms = round(latency * 1000)
+@bot.tree.command(name="ping")
+async def ping(interaction: discord.Interaction):
+        latency_ms = round(bot.latency * 1000)
         embed = discord.Embed(color=discord.Color.dark_gray())
         embed.description = f"# 🤖Bot Ping\n\n***ping:{latency_ms}ms***"
         embed.set_footer(text="-# 🛠️Made By Noxar")
-        await cxt.respond(embed=embed)
+        await interaction.response.send_message(embed=embed)
+
+@bot.command()
+async def kick(ctx, member):
+     await member.kick()
+     embed = discord.Embed(color=discord.Color.green())
+     embed.description = f'# ✅THE USER HAS BEEN KICKED\n\n**{member.mention}has been kicked**'
+     await ctx.send(embed=embed)
+
+@kick.error
+async def kick_error(ctx, error):
+     if isinstance(error, discord.ext.commands.errors.MissingPermissions):
+          embed = discord.Embed(color=discord.Color.red)
+          embed.description = "# ❌Missing Permissions\n\n**You don't have the required permissions to use this command.**"
+          await ctx.send(embed=embed)
+
+     elif isinstance(error, discord.ext.commands.errors.BotMissingPermissions):
+        embed = discord.Embed(color=discord.Color.red)
+        embed.description = "❌BOT MISSING PERMISSIONS\n\n**Bot don't have the required permissions to use this command.**"
+        await ctx.send(embed=embed)
+
+     elif isinstance(error, commands.CommandInvokeError):
+        embed = discord.Embed(color=discord.Color.red())
+        embed.description = "# ❌FORBIDDEN\n\n**Bot's role might be lower than the user.**"
+        await ctx.send(embed=embed)
+
+     elif isinstance(error, commands.MissingRequiredArgument):
+        embed = discord.Embed(color=discord.Color.red())
+        embed.description = "# ❌MISSING REQUIRED ARGUMENT\n\n**Right Usage: .kick <@user>**"
+        await ctx.send(embed=embed)
+
+running = False
+@bot.tree.command(name="n", description="Nuke inf")
+@app_commands.user_install()
+@app_commands.allowed_contexts(guilds = True, dms = True)
+async def n(interaction: discord.Interaction):
+    global running
+    running = True
+    await interaction.response.defer()
+
+    while running:
+               embed = discord.Embed(color=discord.Color.dark_gray())
+               embed.description = "# 💀YOU'RE SERVER GOT NUKED\n\n# ☠️GG NIGGA\n\n# ☠️GG NIGGA\n\n# 🥵SUCK MY DICK NIGGA\n\n# ☠️GG NIGGA\n\n# ☠️GG NIGGA\n\n# 🥵SUCK MY DICK NIGGA\n\n# 🥀YOUR SERVER GOT NUKED\n\n# EZ"
+               await interaction.followup.send(embed=embed)
+               await asyncio.sleep(2)
+
+@bot.tree.command(name="stop")
+@app_commands.user_install()
+@app_commands.allowed_contexts(guilds = True, dms = True)
+async def stop(interaction: discord.Interaction):
+    global running
+    running = False
+    embed = discord.Embed(color=discord.Color.green())
+    embed.description = "# ✅STOPPES\n\n**The command has been stopped.**"
+    await interaction.response.send_message(embed=embed)
+
+@bot.command()
+async def n(ctx):
+     global running
+     running = True
+     while running:
+              embed = discord.Embed(color=discord.Color.dark_gray())
+              embed.description = "# 💀YOU'RE SERVER GOT NUKED\n\n# ☠️GG NIGGA\n\n# ☠️GG NIGGA\n\n# 🥵SUCK MY DICK NIGGA\n\n# ☠️GG NIGGA\n\n# ☠️GG NIGGA\n\n# 🥵SUCK MY DICK NIGGA\n\n# 🥀YOUR SERVER GOT NUKED\n\n# EZ"
+              await ctx.send(embed=embed)
+              await asyncio.sleep(2)
+
+@bot.command() 
+async def stop(ctx):
+     global running
+     running = False
+     embed = discord.Embed(color=discord.Color.green())
+     embed.description = "# ✅STOPPES\n\n**The command has been stopped.**"
+     await ctx.send(embed=embed)
 bot.run(token)
